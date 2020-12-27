ec2c76925a32ee62a1fb5575561b39f3e2addbc5
Ivanstein18 <ivanstein18@yandex.ru>
2020-12-27 19:14:01 +0300

    Добавьте файлы проекта.

---
+++ Новый/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Новый
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int c = (a + b) / 2;
+
+            Console.WriteLine(c);
+
+        }
+    }
+}
