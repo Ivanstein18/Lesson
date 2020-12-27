4f45dcc89618f6ab69faa323cb28206937beec3b
Ivanstein18 <ivanstein18@yandex.ru>
2020-12-27 21:31:27 +0300

    27.12.20

---
+++ DZ4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DZ4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+
+            int value = int.Parse(Console.ReadLine());
+
+            int value2 = value % 2;
+
+            if (value2 == 0)
+                Console.WriteLine("Число четное");
+            else
+                Console.WriteLine("Число не четное");
+
+        }
+    }
+}
---
+++ DZ5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace DZ5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double ferstValue, secondValue;
+            Console.WriteLine("Введите 1-е число");
+            ferstValue = double.Parse(Console.ReadLine());
+           
+            Console.WriteLine("Введите действие: + - / *");
+            char znak = char.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите 2-е число");
+            secondValue = double.Parse(Console.ReadLine());
+
+            switch (znak)
+            {
+                case '+':
+                    Console.WriteLine(ferstValue + secondValue);
+                    break;
+                case '-':
+                    Console.WriteLine(ferstValue - secondValue);
+                    break;
+                case '/':
+                    Console.WriteLine(ferstValue / secondValue);
+                    break;
+                case '*':
+                    Console.WriteLine(ferstValue * secondValue);
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели не знакомый знак действия");
+                    break;
+            }
+
+        }
+    }
+}
