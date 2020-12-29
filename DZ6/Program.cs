36219bbb795990a8145925dc336adde57d4186d3
Ivanstein18 <ivanstein18@yandex.ru>
2020-12-30 02:36:40 +0300

    задача с массивом

--- DZ6/Program.cs
+++ DZ6/Program.cs
@@ -6,7 +6,34 @@ namespace DZ6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int fertValue, secondValue, arrayNumber;
+
+            Console.WriteLine("Введите с какого числа начать считать");
+
+            fertValue = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите до какого числа считать");
+
+            secondValue = int.Parse(Console.ReadLine());
+
+            arrayNumber = secondValue - (fertValue - 1);
+
+            int[] array = new int[arrayNumber];
+
+            Random rnd = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0, 10);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + "\t");
+            }
+            
+
+
+
         }
     }
 }
